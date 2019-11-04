798281bcdc995723c572d852124594d0c994a224
Devlin McClure <mccluredt@gmail.com>
2019-11-04 12:12:58 -0500

    Initial variables for SlingshotBody script

--- Assets/Scripts/SlingshotBody.cs
+++ Assets/Scripts/SlingshotBody.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class SlingshotBody : MonoBehaviour
 {
+    //Object the player has grabbed
+    public GameObject grabbedObject;
+    //The player's body
+    public GameObject body;
+
+    //Speed at which the player can walk when holding something
+    public float holdSpeed;
+
+    //Representation of the player's body when stretching
+    public LineRenderer stretchedBody;
+
+    
+    //How far the body can go
+    public float maxDistance;
+    //How far away from the player the body is
+    private float currentDistance;
+
     // Start is called before the first frame update
     void Start()
     {
