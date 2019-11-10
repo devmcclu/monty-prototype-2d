9d98b3b4b9af77b88a2d8780a55f57e683e896ef
Devlin McClure <mccluredt@gmail.com>
2019-11-09 19:48:33 -0500

    Slow down the player when they get big

--- Assets/Scripts/LargeMode.cs
+++ Assets/Scripts/LargeMode.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LargeMode : MonoBehaviour
-{
+{    
+    //Floats for keeping track of old and new scales
     public Vector3 origScale;
     public Vector3 largeScale = new Vector3(10f, 10f, 10f);
+    //Floats for keeping track of new and old speeds
+    public float oldSpeed;
+    public float slowSpeed;
+    
+    public MovementScript moveScript;
 
     // Start is called before the first frame update
     void Start()
     {
         origScale = this.transform.localScale;
+        moveScript = this.GetComponent<MovementScript>();
         //this.transform.localScale = largeScale;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetAxis("Fire3") > 0){
+            this.transform.position += new Vector3(0f, 0.5f, 0f);
+            this.transform.localScale = largeScale;
+            oldSpeed = moveScript.speed;
+            moveScript.speed = slowSpeed;
+        }
     }
 }
