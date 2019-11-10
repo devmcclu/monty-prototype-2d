db6f228e8ff0d9b53b7ba1e603a31363d2a9add1
Devlin McClure <mccluredt@gmail.com>
2019-11-09 19:31:26 -0500

    Test out scaling of the player object

---
+++ Assets/Scripts/LargeMode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeMode : MonoBehaviour
+{
+    public Vector3 origScale;
+    public Vector3 largeScale = new Vector3(10f, 10f, 10f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origScale = this.transform.localScale;
+        //this.transform.localScale = largeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
