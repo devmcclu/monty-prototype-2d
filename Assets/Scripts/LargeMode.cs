df82456f059b363c0b9757f80d8542171400698a
Devlin McClure <mccluredt@gmail.com>
2019-11-11 12:25:13 -0500

    Make a function for handling size increase

--- Assets/Scripts/LargeMode.cs
+++ Assets/Scripts/LargeMode.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class LargeMode : MonoBehaviour
 {    
     //Floats for keeping track of old and new scales
-    public Vector3 origScale;
+    private Vector3 origScale;
     public Vector3 largeScale = new Vector3(10f, 10f, 10f);
     //Floats for keeping track of new and old speeds
     public float oldSpeed;
     public float slowSpeed;
+
+    public bool isLarge = false;
     
     public MovementScript moveScript;
 
@@ -24,11 +26,17 @@ public class LargeMode : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetAxis("Fire3") > 0){
-            this.transform.position += new Vector3(0f, 0.5f, 0f);
+        // if (Input.GetAxis("Fire3") > 0){
+        //     BecomeLarge();
+        // }
+    }
+
+    public void BecomeLarge()
+    {
+            this.transform.position += new Vector3(0f, 2f, 0f);
             this.transform.localScale = largeScale;
             oldSpeed = moveScript.speed;
             moveScript.speed = slowSpeed;
-        }
+            isLarge = true;
     }
 }
